2e528d516e8268c9d1fed184d8058f6557f34373
Chandra13July <chandra13july2004@gmail.com>
2025-01-16 11:44:50 +0700

    Kode Function

--- data/Person.php
+++ data/Person.php
@@ -5,4 +5,9 @@ class Person
     var string $name;
     var ?string $address = null;
     var string $country = "Indonesia";
+
+    function sayHello(string $name)
+    {
+        echo "Hallo $name" . PHP_EOL;
+    }
 }
