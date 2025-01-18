c80143ab6e94f3f6bbf3909689b8bdd7edbe3c0d
Chandra13July <chandra13july2004@gmail.com>
2025-01-18 23:23:42 +0700

    Kode Class Product

---
+++ data/Product.php
@@ -0,0 +1,13 @@
+<?php 
+
+class Product 
+{
+    private string $name;
+    private int $price;
+
+    public function __construct(string $name, int $price)
+    {
+        $this->name = $name;
+        $this->price = $price;
+    }
+}
